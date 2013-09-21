243245edcbb9c7dde3ef7a3402df16f3f4b3d974
Daniel Bachhuber <d@danielbachhuber.com>
2013-09-20 17:08:06 -0700

    First pass at abstracting the WP Remote CLI commands to a new pattern

--- wp-remote-cli.php
+++ wp-remote-cli.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * A series of WP Remote commands for WP-CLI
+ */
+require_once dirname( __FILE__ ) . '/commands/class-wp-remote-command.php';
+require_once dirname( __FILE__ ) . '/commands/class-wp-remote-plugin-command.php';
+
+/**
  * Manage your WordPress sites using WP Remote.
  */
 class WP_CLI_Remote_Command extends WP_CLI_Command {
@@ -36,85 +42,6 @@ class WP_CLI_Remote_Command extends WP_CLI_Command {
 		);
 
 	static $unknown_error_message = "An error occurred that we don't have code for. Please get in touch with WP Remote support or submit a pull request.";
-
-	/**
-	 * List all of the plugins installed on a given site.
-	 * 
-	 * @subcommand plugin-list
-	 * @synopsis <site-id> [--fields=<fields>] [--format=<format>]
-	 */
-	public function plugin_list( $args, $assoc_args ) {
-
-		list( $site_id ) = $args;
-
-		$defaults = array(
-				'fields'      => implode( ',', $this->plugin_fields ),
-				'format'      => 'table',
-			);
-		$assoc_args = array_merge( $defaults, $assoc_args );
-
-		$this->list_plugins_or_themes_for_site( 'plugins', $site_id, $assoc_args );
-	}
-
-	/**
-	 * Install a given plugin on a given site.
-	 *
-	 * @subcommand plugin-install
-	 * @synopsis <site-id> <plugin-slug> [--version=<version>]
-	 */
-	public function plugin_install( $args, $assoc_args ) {
-
-		list( $site_id, $plugin_slug ) = $args;
-		$this->perform_plugin_or_theme_action_for_site( 'plugin', 'install', $plugin_slug, $site_id, $assoc_args );
-	}
-
-	/**
-	 * Activate a given plugin on a given site.
-	 *
-	 * @subcommand plugin-activate
-	 * @synopsis <site-id> <plugin-slug>
-	 */
-	public function plugin_activate( $args ) {
-
-		list( $site_id, $plugin_slug ) = $args;
-		$this->perform_plugin_or_theme_action_for_site( 'plugin', 'activate', $plugin_slug, $site_id );
-	}
-
-	/**
-	 * Deactivate a given plugin on a given site.
-	 *
-	 * @subcommand plugin-deactivate
-	 * @synopsis <site-id> <plugin-slug>
-	 */
-	public function plugin_deactivate( $args ) {
-
-		list( $site_id, $plugin_slug ) = $args;
-		$this->perform_plugin_or_theme_action_for_site( 'plugin', 'deactivate', $plugin_slug, $site_id );
-	}
-
-	/**
-	 * Update a given plugin on a given site.
-	 *
-	 * @subcommand plugin-update
-	 * @synopsis <site-id> <plugin-slug>
-	 */
-	public function plugin_update( $args ) {
-
-		list( $site_id, $plugin_slug ) = $args;
-		$this->perform_plugin_or_theme_action_for_site( 'plugin', 'update', $plugin_slug, $site_id );
-	}
-
-	/**
-	 * Uninstall a given plugin on a given site.
-	 *
-	 * @subcommand plugin-uninstall
-	 * @synopsis <site-id> <plugin-slug>
-	 */
-	public function plugin_uninstall( $args, $assoc_args ) {
-
-		list( $site_id, $plugin_slug ) = $args;
-		$this->perform_plugin_or_theme_action_for_site( 'plugin', 'uninstall', $plugin_slug, $site_id );
-	}
 
 	/**
 	 * List all of the themes installed on a given site.
@@ -507,4 +434,4 @@ class WP_CLI_Remote_Command extends WP_CLI_Command {
 
 
 }
-WP_CLI::add_command( 'remote', 'WP_CLI_Remote_Command' );
+WP_CLI::add_command( 'wpr', 'WP_CLI_Remote_Command' );
