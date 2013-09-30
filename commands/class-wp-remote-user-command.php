734c628d7e6f5fac2b98795e287b300de1c5557b
Daniel Bachhuber <d@danielbachhuber.com>
2013-09-29 19:03:53 -0700

    API returns a proper response now

--- commands/class-wp-remote-user-command.php
+++ commands/class-wp-remote-user-command.php
@@ -62,15 +62,6 @@ class WP_Remote_User_Command extends WP_Remote_Command {
 			if ( !is_object( $user ) )
 				return $user;
 
-			// Sadly, response isn't a WP_User object
-			$user->user_login = $user->data->user_login;
-			$user->user_nicename = $user->data->user_nicename;
-			$user->user_email = $user->data->user_email;
-			$user->user_url = $user->data->user_url;
-			$user->user_registered = $user->data->user_registered;
-			$user->user_status = $user->data->user_status;
-			$user->display_name = $user->data->display_name;
-
 			$user->roles = implode( ',', $user->roles );
 
 			return $user;
@@ -146,7 +137,167 @@ class WP_Remote_User_Command extends WP_Remote_Command {
 			WP_CLI::success( "Created user {$response->ID}." );
 			// Password was generated on remote site.
 			if ( ! $assoc_args['user_pass'] )
-				WP_CLI::line( "Password: {$response->data->user_pass}" );
+				WP_CLI::line( "Password: {$response->user_pass}" );
+		}
+	}
+
+	/**
+	 * Update a User on a remote Site.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <user>
+	 * : The user login or ID of the user to update.
+	 *
+	 * --<field>=<value>
+	 * : One or more fields to update. For accepted fields, see wp_update_user().
+	 * 
+	 * [--site-id=<site-id>]
+	 * : Site to run the command on.
+	 */
+	public function update( $args, $assoc_args ) {
+
+		list( $user ) = $args;
+
+		$site_id = $assoc_args['site-id'];
+		unset( $assoc_args['site-id'] );
+
+		$this->set_account();
+
+		$args = array(
+			'endpoint' => 'site/' . (int)$site_id . '/user/' . $user,
+			'method'   => 'POST',
+			'body'     => $assoc_args,
+			);
+		$response = $this->api_request( $args );
+
+		if ( is_wp_error( $response ) )
+			WP_CLI::error( $response->get_error_message() );
+
+		WP_CLI::success( "User updated." );
+	}
+
+	/**
+	 * Get a single User on a remote Site.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <user>
+	 * : User ID or user login.
+	 *
+	 * [--field=<field>]
+	 * : Instead of returning the whole user, returns the value of a single field.
+	 *
+	 * [--format=<format>]
+	 * : The format to use when printing the user; acceptable values:
+	 *
+	 *     **table**: Outputs all fields of the user as a table.
+	 *
+	 *     **json**: Outputs all fields in JSON format.
+	 * 
+	 * [--site-id=<site-id>]
+	 * : Site to run the command on.
+	 */
+	public function get( $args, $assoc_args ) {
+
+		list( $user ) = $args;
+
+		$site_id = $assoc_args['site-id'];
+		unset( $assoc_args['site-id'] );
+
+		$defaults = array(
+			'format'     => 'table',
+			);
+		$assoc_args = array_merge( $defaults, $assoc_args );
+		
+		$this->set_account();
+
+		$args = array(
+			'endpoint' => 'site/' . (int)$site_id . '/user/' . $user,
+			'method'   => 'GET',
+			);
+		$response = $this->api_request( $args );
+
+		if ( is_wp_error( $response ) )
+			WP_CLI::error( $response->get_error_message() );
+
+		$user = $response;
+
+		$user_data = (array) $user;
+		$user_data['roles'] = implode( ', ', $user->roles );
+
+		if ( isset( $assoc_args['field'] ) ) {
+			$this->show_single_field( (object) $user_data, $assoc_args['field'] );
+		} else {
+			$this->show_multiple_fields( $user_data, $assoc_args );
+		}
+	}
+
+	/**
+	 * Delete a User on a remote Site.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <user>
+	 * : The user login or ID of the user to delete.
+	 * 
+	 * [--site-id=<site-id>]
+	 * : Site to run the command on.
+	 */
+	public function delete( $args, $assoc_args ) {
+
+		list( $user ) = $args;
+
+		$site_id = $assoc_args['site-id'];
+		unset( $assoc_args['site-id'] );
+		
+		$this->set_account();
+
+		$args = array(
+			'endpoint' => 'site/' . (int)$site_id . '/user/' . $user,
+			'method'   => 'DELETE',
+			);
+		$response = $this->api_request( $args );
+
+		if ( is_wp_error( $response ) )
+			WP_CLI::error( $response->get_error_message() );
+
+		WP_CLI::success( "User deleted." );
+
+	}
+
+	private function show_multiple_fields( $user_data, $assoc_args ) {
+		switch ( $assoc_args['format'] ) {
+
+		case 'table':
+			\WP_CLI\Utils\assoc_array_to_table( $user_data );
+			break;
+
+		case 'json':
+			WP_CLI::print_value( $user_data, $assoc_args );
+			break;
+
+		default:
+			\WP_CLI::error( "Invalid format: " . $assoc_args['format'] );
+			break;
+
+		}
+	}
+
+	protected function show_single_field( $user, $field ) {
+		$value = null;
+
+		foreach ( array( $field, 'user_' . $field ) as $key ) {
+			if ( isset( $user->$key ) ) {
+				$value = $user->$key;
+				break;
+			}
+		}
+
+		if ( null === $value ) {
+			\WP_CLI::error( "Invalid user field: $field." );
+		} else {
+			\WP_CLI::print_value( $value );
 		}
 	}
 
